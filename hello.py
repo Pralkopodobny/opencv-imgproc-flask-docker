a64bf2abb549655ff89243804d74d0749e2a6a88
rafal <rafal1behrendt@gmail.com>
2021-03-17 00:33:45 +0100

    init demo

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, send_file
+from flask_restful import Api, Resource, reqparse
+import requests
+import werkzeug
+import os
+import io
+from PIL import Image
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,7 +13,27 @@ class HelloWorld(Resource):
     def get(self):
         return {"data": 'Hello World!'}
 
+class LoadImage(Resource):
+    def post(self):
+        parse = reqparse.RequestParser()
+        parse.add_argument('image', type=werkzeug.datastructures.FileStorage, location='files')
+        args = parse.parse_args()
+        image_file = args['image']
+        image_file.save("newImage.jpg")
+        return send_file(image_file, mimetype='image/jpg')
+
+    def get(self):
+        file_path = os.path.join(app.root_path, "newImage.jpg")
+        return_data = io.BytesIO()
+        with open(file_path, 'rb') as fo:
+            return_data.write(fo.read())
+        return_data.seek(0)
+        os.remove(file_path)
+        return send_file(return_data, mimetype='image/jpg', attachment_filename='image.jpg')
+
+
 api.add_resource(HelloWorld, "/hello")
+api.add_resource(LoadImage, "/image")
 
 if __name__ == "__main__":
     app.run(debug=True)
