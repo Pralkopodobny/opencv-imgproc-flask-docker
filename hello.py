c7ae256a3caa2184e71b910b86090375151280bd
rafal <rafal1behrendt@gmail.com>
2021-03-29 16:36:26 +0200

    init storage free

--- hello.py
+++ hello.py
@@ -1,40 +1,54 @@
-from flask import Flask, send_file
-from flask_restful import Api, Resource, reqparse
-import requests
-import werkzeug
-import os
-import io
+from flask import Flask, render_template , request , jsonify, make_response
+from flask_restful import Resource, Api
 from PIL import Image
+import os , io , sys
+import numpy as np 
+import cv2
+import base64
+
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": 'Hello World!'}
 
+class HomePage(Resource):
+    def get(self):
+        return make_response(render_template('index.html'))
+        
 class LoadImage(Resource):
     def post(self):
-        parse = reqparse.RequestParser()
-        parse.add_argument('image', type=werkzeug.datastructures.FileStorage, location='files')
-        args = parse.parse_args()
-        image_file = args['image']
-        image_file.save("newImage.jpg")
-        return send_file(image_file, mimetype='image/jpg')
-
-    def get(self):
-        file_path = os.path.join(app.root_path, "newImage.jpg")
-        return_data = io.BytesIO()
-        with open(file_path, 'rb') as fo:
-            return_data.write(fo.read())
-        return_data.seek(0)
-        os.remove(file_path)
-        return send_file(return_data, mimetype='image/jpg', attachment_filename='image.jpg')
+        print("YEAH", file = sys.stderr)
+        file = request.files['image'].read()
+        npimg = np.fromstring(file, np.uint8)
+        img = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
+        
+        img = Image.fromarray(img.astype("uint8"))
+        rawBytes = io.BytesIO()
+        img.save(rawBytes, "JPEG")
+        rawBytes.seek(0)
+        img_base64 = base64.b64encode(rawBytes.read())
+        return jsonify({'status':str(img_base64)})
 
 
-api.add_resource(HelloWorld, "/hello")
+class Test(Resource):
+    def post(self):
+	    print("log: got at test" , file=sys.stderr)
+	    return jsonify({'status':'succces'})
+
+
+@app.after_request
+def after_request(response):
+    print("log: setting cors" , file = sys.stderr)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+
+api.add_resource(HomePage, "/")
 api.add_resource(LoadImage, "/image")
+api.add_resource(Test, "/test")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
