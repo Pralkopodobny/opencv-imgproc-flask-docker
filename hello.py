e6c5a2772b7aa17441160261b975a279fa957450
Pralkopodobny <43888534+Pralkopodobny@users.noreply.github.com>
2021-03-30 00:24:19 +0200

    Add core functionality

--- hello.py
+++ hello.py
@@ -9,20 +9,18 @@ import imageprocessor as improc
 app = Flask(__name__)
 api = Api(app)
 
+def get_image(filename):
+    parse = reqparse.RequestParser()
+    parse.add_argument('image', type=werkzeug.datastructures.FileStorage, location='files')
+    args = parse.parse_args()
+    image_file = args['image']
+    image_file.save(filename)
+
 class HelloWorld(Resource):
     def get(self):
         return {"data": 'Hello World!'}
 
 class LoadImage(Resource):
-    def post(self):
-        parse = reqparse.RequestParser()
-        parse.add_argument('image', type=werkzeug.datastructures.FileStorage, location='files')
-        args = parse.parse_args()
-        image_file = args['image']
-        image_file.save("newImage.jpg")
-        improc.to_grayscale("newImage.jpg")
-        return {'id': "newImage"}
-
     def get(self):
         file_path = os.path.join(app.root_path, "newImage.jpg")
         return_data = io.BytesIO()
@@ -32,10 +30,116 @@ class LoadImage(Resource):
         os.remove(file_path)
         return send_file(return_data, mimetype='image/jpg', attachment_filename='image.jpg')
 
+class Grayscale(Resource):
+    def post(self):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.to_grayscale(image)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
+
+class Median(Resource):
+    def post(self, ksize):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.median_blur(image, ksize)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
+
+class Average(Resource):
+    def post(self, ksize_x, ksize_y):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.average_blur(image, ksize_x, ksize_y)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
+
+class GaussianBlur(Resource):
+    def post(self, ksize_x, ksize_y):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.gaussian_blur(image, ksize_x, ksize_y)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
+
+class Bilateral(Resource):
+    def post(self, d, sigma):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.bilateral_filter(image, d, sigma)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
+
+class GlobalThresh(Resource):
+    def post(self, threshold, value):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.global_threshold(image, threshold, value)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
+
+class MeanThreshold(Resource):
+    def post(self, blocksize, c, value):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.mean_threshold(image, blocksize, c, value)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
+
+class GaussianThreshold(Resource):
+    def post(self, blocksize, c, value):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.gaussian_threshold(image, blocksize, c, value)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
+
+class Sobel(Resource):
+    def post(self, dx, dy, ksize, delta):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.sobel(image, dx, dy, ksize, delta)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
+
+class Laplacian(Resource):
+    def post(self, ksize, delta):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.laplacian(image, ksize, delta)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
+
+class Canny(Resource):
+    def post(self, threshold1, threshold2):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.canny_edge_detection(image, threshold1, threshold2)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
+
+class FrontalFace(Resource):
+    def post(self, min_neighbours, scale):
+        get_image("newImage.jpg")
+        image, err = improc.read_image("newImage.jpg")
+        image, err = improc.haar_frontal_face_detection(image, min_neighbours, scale)
+        improc.write_image(image, "newImage.jpg")
+        return {'id': "newImage", 'error': err}
 
 api.add_resource(HelloWorld, "/hello")
 api.add_resource(LoadImage, "/image")
-
+api.add_resource(Grayscale, "/gray")
+api.add_resource(Median, "/median/<int:ksize>")
+api.add_resource(Average, "/average/<int:ksize_x>/<int:ksize_y>")
+api.add_resource(GaussianBlur, "/gauss/<int:ksize_x>/<int:ksize_y>")
+api.add_resource(Bilateral, "/bilateral/<int:d>/<float:sigma>")
+api.add_resource(GlobalThresh, "/thresh/global/<int:threshold>/<int:value>")
+api.add_resource(MeanThreshold, "/thresh/mean/<int:blocksize>/<float:c>/<int:value>")
+api.add_resource(GaussianThreshold, "/thresh/gauss/<int:blocksize>/<float:c>/<int:value>")
+api.add_resource(Sobel, "/sobel/<int:dx>/<int:dy>/<int:ksize>/<float:delta>")
+api.add_resource(Laplacian,"/laplacian/<int:ksize>/<float:delta>")
+api.add_resource(Canny, "/canny/<int:threshold1>/<int:threshold2>")
+api.add_resource(FrontalFace, "/frontal/<int:min_neighbours>/<float:scale>")
 if __name__ == "__main__":
     app.run(debug=True)
     
--- imageprocessor.py
+++ imageprocessor.py
@@ -1,6 +1,115 @@
 from cv2 import cv2
+import numpy as np
+haar_cascade = None
+haar_cascade_loaded = False
+try:
+    haar_cascade = cv2.CascadeClassifier('./FaceDetectionAssets/haarcascade_frontalface_default.xml')
+    haar_cascade_loaded = True
+except:
+    print('haar cascade could not be loaded')
 
-def to_grayscale(filename):
-    image = cv2.imread(filename, cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(filename, gray)
+
+def read_image(filename : str):
+    result = cv2.imread(filename)
+    return result, None
+
+def write_image(image, filename: str):
+    cv2.imwrite(filename, image)
+
+def to_grayscale(image):
+    result = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return result, None
+
+def median_blur(image, ksize : int):
+    if ksize < 1 or ksize%2 == 0:
+        return image, "Kernel size should be positive odd number"
+    result = cv2.medianBlur(image, ksize=ksize)
+    return result, None
+
+def gaussian_blur(image, ksize_x : int, ksize_y : int):
+    if ksize_x < 1 or ksize_y < 1 or ksize_x%2 == 0 or ksize_y%2 == 0:
+        return image, "Kernel sizes should be positive odd numbers"
+    result = cv2.GaussianBlur(image, (ksize_x, ksize_y), 0)
+    return result, None
+
+def average_blur(image, ksize_x : int, ksize_y : int):
+    if ksize_x < 1 or ksize_y < 1 or ksize_x%2 == 0 or ksize_y%2 == 0:
+        return image, "Kernel sizes should be positive odd numbers"
+    result = cv2.blur(image, (ksize_x, ksize_y))
+    return result, None
+
+def bilateral_filter(image, d : int, sigma : float):
+    if d < 1:
+        return image, "d should be positive"
+    if sigma <= 0:
+        return image, "Sigma should be positive"
+    result = cv2.bilateralFilter(image, d, sigma, sigma)
+    return result, None
+
+def global_threshold(image, threshold : int, value : int):
+    if threshold < 0 or threshold > 255:
+        return image, "Threshold should be between 0 and 255"
+    if value < 0 or value > 255:
+        return image, "Value should be between 0 and 255"
+    result, _ = to_grayscale(image)
+    _, result = cv2.threshold(result, threshold, value, cv2.THRESH_BINARY)
+    return result, None
+
+def mean_threshold(image, blocksize : int, c : float, value : int):
+    if blocksize < 1 or blocksize%2 == 0:
+        return image, "Blocksize should be positive odd number"
+    if value < 0 or value > 255:
+        return image, "Value should be between 0 and 255"
+    result, _ = to_grayscale(image)
+    result = cv2.adaptiveThreshold(result, value, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blocksize, c)
+    return result, None
+    
+def gaussian_threshold(image, blocksize : int, c : float, value : int):
+    if blocksize < 1 or blocksize%2 == 0:
+        return image, "Blocksize should be positive odd number"
+    if value < 0 or value > 255:
+        return image, "Value should be between 0 and 255"
+    result, _ = to_grayscale(image)
+    result = cv2.adaptiveThreshold(result, value, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, blocksize, c)
+    return result, None
+
+def sobel(image, dx : int, dy : int, ksize : int, delta : float):
+    if not (dx == 1 or dx == 0) or not (dy == 1 or dy == 0):
+        return image, "dx and dy should be 1 or 0"
+    if ksize < 1 or ksize%2 == 0:
+        return image, "Kernel size should be positive odd number"
+    result, _ = to_grayscale(image)
+    result = cv2.Sobel(result, cv2.CV_64F, dx, dy, ksize=ksize, delta=delta)
+    result = np.absolute(result)
+    result = np.uint8(result)
+    return result, None
+
+def laplacian(image, ksize : int, delta : float):
+    if ksize < 1 or ksize % 2 == 0:
+        return image, "Kernel size should be positive odd number"
+    result, _ = to_grayscale(image)
+    result = cv2.Laplacian(result, cv2.CV_64F, ksize=ksize, delta=delta)
+    result = np.absolute(result)
+    result = np.uint8(result)
+    return result, None
+
+def canny_edge_detection(image, threshold1 : int, threshold2 : int):
+    if threshold1 < 0 or threshold1 > 255 or threshold2 < 0 or threshold2 > 255:
+        return image, "Thresholds should be between 0 and 255"
+    result, _ = to_grayscale(image)
+    result = cv2.Canny(result, threshold1, threshold2)
+    return result, None
+
+def haar_frontal_face_detection(image, min_neighbours : int, scale : float):
+    if not haar_cascade_loaded:
+        return image, "Server error, service not available"
+    if min_neighbours < 1:
+        return image, "min_neighbours should be positive number"
+    if scale <= 1:
+        return image, "scale should be greater then 1"
+    gray, _ = to_grayscale(image)
+
+    detected_faces = haar_cascade.detectMultiScale(gray, minNeighbors=min_neighbours, scaleFactor=scale)
+    for (x, y, w, h) in detected_faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+    return image, None
